2e3481bd12ffa8fe44e60b4705a470f81f5b2cdd
ProjetsDevFanny <contact.fanny.simon@gmail.com>
2026-06-26 16:30:38 +0200

    test(cypress.config) : retries specifications

--- cypress.config.js
+++ cypress.config.js
@@ -2,13 +2,17 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   // allowCypressEnv: false,
+  e2e: {
+    "retries": {
+      "runMode": 0,
+      "openMode": 2
+    },
 
   env: {
     testEnvURL: 'https://example.cypress.io',
     apiUrl: "http://localhost:8081"
   },
-  e2e: {
-    setupNodeEvents(on, config) {
+  setupNodeEvents(on, config) {
       // implement node event listeners here
     },
     baseURL: 'http://localhost:8080/'
